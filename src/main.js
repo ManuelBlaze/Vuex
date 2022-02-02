c25e3f1e453c5865ee7274a1b7e2870efdcce9fb
Manuel Escobar <manuel.escobar1217@gmail.com>
2022-02-02 11:38:21 -0500

    Defining store with Vuex

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue';
 
 import App from './App.vue';
+import Store from './store';
 
-const app = createApp(App);
-
-app.mount('#app');
+createApp(App).use(Store).mount('#app');
