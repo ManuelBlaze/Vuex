acf53b80e4b8895806fe4de62c2b3e1ab11a2d60
Manuel Escobar <manuel.escobar1217@gmail.com>
2022-02-02 12:06:53 -0500

    Adding mutations

--- src/store.js
+++ src/store.js
@@ -2,6 +2,13 @@ import { createStore } from 'vuex';
 
 export default createStore({
   state() {
-    return { counter: 0 };
+    return {
+      counter: 0,
+    };
+  },
+  mutations: {
+    addOne(state) {
+      state.counter++;
+    },
   },
 });
